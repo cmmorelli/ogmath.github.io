1e13e8108df7c6aec289c55e0021a9bc4555e20d
Michael Morelli <mmorelli@live.com>
2020-10-27 16:58:00 +0000

    v

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
       span.underline{text-decoration: underline;}
       div.column{display: inline-block; vertical-align: top; width: 50%;}
   </style>
-  <link rel="stylesheet" href="pandoc1.css" />
+  <link rel="stylesheet" href="pandoc.css" />
   <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" type="text/javascript"></script>
   <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
   <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js?"></script>
