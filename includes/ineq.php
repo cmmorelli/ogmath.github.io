2d2d94f28ebc84d6915ac364ce51361ebe95f989
Michael Morelli <mmorelli@ogmath.com>
2020-11-16 11:47:03 -0600

    update

--- includes/ineq.php
+++ includes/ineq.php
@@ -7,16 +7,25 @@
         </head>
         <title>Inequalities</title>
         <body>
-                <p>
+		<p>
+		<h2>Learning Goals</h2>
+			You will learn how to <code class="verb">identify</code> 
+			the <code class="noun">domain</code> of, 
+			<code class="verb">solve</code>, and <code class="verb">graph</code> 
+			<i>single variable</i> <b><code class="noun">linear inequalities</code></b>.
                 <h2>TEKS</h2>
-                <h3>5B</h3>
-                <blockquote>
-                        Solve linear inequalities in one variable,
-                        including those for which the application
-                        of the distributive property is necessary
-                        and for which variables are included on
-                        both sides.
-		</blockquote>
+		<div class="teks">
+			<h3>A1.5B</h3>
+                	<blockquote>
+				<code class="verb">Solve</code> 
+				<code class="noun">linear inequalities</code> 
+				<code class="prep">in <i>one variable</i></code>
+				, including those for which the application 
+				of the <code class="noun">distributive property</code> 
+				is necessary and for which <code class="noun">variables
+				</code> are included <code class= "prep">on both sides</code>.
+			</blockquote>
+		</div>
 	</body>
 </html>
 
