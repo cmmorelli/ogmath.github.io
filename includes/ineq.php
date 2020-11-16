071058627a5c45fc0236d119b8a529af97b88843
Michael Morelli <mmorelli@ogmath.com>
2020-11-16 08:17:02 -0600

    new addition

---
+++ includes/ineq.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <?php include '../header.js' ?>
+                <link rel="stylesheet" style="text/css"
+                href="../pandoc.css">
+        </head>
+        <title>Inequalities</title>
+        <body>
+                <p>
+                <h2>TEKS</h2>
+                <h3>5B</h3>
+                <blockquote>
+                        Solve linear inequalities in one variable,
+                        including those for which the application
+                        of the distributive property is necessary
+                        and for which variables are included on
+                        both sides.
+		</blockquote>
+	</body>
+</html>
+
