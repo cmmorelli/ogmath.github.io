8a193ab7503b89712c9aacea1395d75c51bf6e14
Michael Morelli <mike@ogmath.com>
2020-11-17 12:32:21 -0600

    added jsxgraph

--- header.js
+++ header.js
@@ -1,4 +1,13 @@
+<!--MathJax-->
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js?"></script>
+
+<!--JSXGraph-->
+<script type="text/javascript" charset="UTF-8"
+ src="//cdnjs.cloudflare.com/ajax/libs/jsxgraph/1.1.0/jsxgraphcore.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/jsxgraph/1.1.0/jsxgraph.css" />
+
+<!--CSS-->
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu+Mono" />
 <link rel="stylesheet" href="pandoc.css">
+<link rel="stylesheet" href="jsxgraph.css">
