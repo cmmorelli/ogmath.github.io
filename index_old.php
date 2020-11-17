c62013b3e383d6a353dbfef37ca9e5b465ce52d0
root <root@ogmath.com>
2020-11-16 20:03:57 -0600

    Update

---
+++ index_old.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="" xml:lang="">
+<head>
+  <meta charset="utf-8" />
+  <meta name="generator" content="pandoc" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+  <title>OGMATH</title>
+  <style>
+    code{white-space: pre-wrap;}
+    span.smallcaps{font-variant: small-caps;}
+    span.underline{text-decoration: underline;}
+    div.column{display: inline-block; vertical-align: top; width: 50%;}
+    div.hanging-indent{margin-left: 1.5em; text-indent: -1.5em;}
+    ul.task-list{list-style: none;}
+  </style>
+  <link rel="stylesheet" href="pandoc.css" />
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+  <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js?"></script>
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu+Mono" />
+  <link rel="stylesheet" href="pandoc.css">
+</head>
+<body>
+<title>
+OGMATH
+</title>
+<p><a href="https://ogmath.com/index.php"><span>🧙🏻</span>OGMATH</a></p>
+<h1 id="morelli-algebra-1-notes">Morelli Algebra 1 Notes</h1>
+<p><a href="https://student.works.kutasoftware.com" title="Kuta Works Student Login">Kuta</a> | <a href="https://youtube.com/c/AthenianStranger" title="Athenian Stranger YouTube Tutorial Videos">YouTube</a> | <a href="https://bit.ly/desmos-test-mode-chrome-app" title="Desmos Test Mode Chrome App Extension">Desmos</a> | <a href="https://ogmath.com/index.md" title="Markdown File for This Webpage">Code</a> | <a href="https://github.com/ogmath/" title="My GitHub Page">Git</a></p>
+<?php ini_set('display_errors',1);?>
+<?php error_reporting(E_ALL);?>
+<?php
+    include './includes/exponents.html';
+?>
+<?php include ("./includes/desmos.html");?>
+<h2 id="contact-me">Contact Me</h2>
+<p>Email me: <a href="mailto:contact@ogmath.com">contact@ogmath.com</a></p>
+<h2 id="dedication">Dedication</h2>
+<p><code>This page is dedicated to my students.</code></p>
+<center>
+<a href="https://ogmath.com/index.php"><span>🧙🏻</span>OGMATH</a>
+<center>
+</body>
+</html>
