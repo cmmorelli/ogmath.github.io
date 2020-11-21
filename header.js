ed31cbff30d6b5c91804a5255e1e3256cf691338
root <root@ogmath.com>
2020-11-21 17:21:32 -0600

    Update

--- header.js
+++ header.js
@@ -1,27 +1,28 @@
 <!--MathJax-->
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js?"></script>
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js?"></script>
 
-		<script>
-			window.MathJax = {
-			tex: {
-			inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-			displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
-			packages: ['base', 'ams']
-			},
-			options: {
-			ignoreHtmlClass: 'tex2jax_ignore',
-			processHtmlClass: 'tex2jax_process'
-			}
-			};
-		</script>
+<!--JSXGraph MathJax Specifications-->
+	<script>
+		window.MathJax = {
+		tex: {
+		inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+		displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+		packages: ['base', 'ams']
+		},
+		options: {
+		ignoreHtmlClass: 'tex2jax_ignore',
+		processHtmlClass: 'tex2jax_process'
+		}
+		};
+	</script>
 		
 <!--JSXGraph-->
-<script type="text/javascript" charset="UTF-8"
- src="//cdnjs.cloudflare.com/ajax/libs/jsxgraph/1.1.0/jsxgraphcore.js"></script>
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/jsxgraph/1.1.0/jsxgraph.css" />
+	<script type="text/javascript" charset="UTF-8"
+	 src="//cdnjs.cloudflare.com/ajax/libs/jsxgraph/1.1.0/jsxgraphcore.js"></script>
+	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/jsxgraph/1.1.0/jsxgraph.css" />
 
 <!--CSS-->
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu+Mono" />
-<link rel="stylesheet" href="pandoc.css">
-<link rel="stylesheet" href="jsxgraph.css">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu+Mono" />
+	<link rel="stylesheet" href="pandoc.css">
+	<link rel="stylesheet" href="jsxgraph.css">
