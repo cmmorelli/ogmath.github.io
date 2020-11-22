13a0b59c572b7cd1c64c9df11c10d16e93d9032a
root <root@ogmath.com>
2020-11-21 23:37:09 -0600

    Update

--- includes/mathquill.php
+++ includes/mathquill.php
@@ -1,29 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-<!--        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Run  Mathquill</title>
-
-
-        <!-- YOU NEED  mathquill.css , jquery and mathquill.js SEARCH AND PICK IT SOMEWHERE, SEARCH ON THE WEB IF THIS DOWN  NOT RUN-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.css"/> 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.js"></script>
-
-        <!-- INFORM  THE LATEST AVERSION INTERFACE WITH THIS CODE   -->
-        <script>
-            var MQ = MathQuill.getInterface(2);
-        </script>
--->
-
 	<?php include "https://ogmath.com/header.js";?>
-
     </head>
-
     <body>
-
 	<p>Type math here: <span id="math-field"></span></p>
 	<p>LaTeX of what you typed: <span id="latex"></span></p>
 
@@ -41,11 +21,6 @@
 				  }
 		});
 	</script>
-
-	<script>
-		console.log(latexSpan)
-	</script>
-
     </body>
 </html>
 
