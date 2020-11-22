7e6e108bced640fb8a2f317c6b35fb420c882c3a
root <root@ogmath.com>
2020-11-22 13:08:37 -0600

    Update

--- includes/mathquill.php
+++ includes/mathquill.php
@@ -6,7 +6,7 @@
     <body>
 	<p>Type math here: <span id="math-field"></span></p>
 	<p>LaTeX of what you typed: <span id="latex"></span></p>
-	<button id="button">Confirm</button>
+	<button id="button" onclick="sendEmail()">Confirm</button>
 	<script>
 		var mathFieldSpan = document.getElementById('math-field');
 		var latexSpan = document.getElementById('latex');
@@ -20,6 +20,11 @@
 						  }
 				  }
 		});
+
+		function sendEmail() {
+			var email = document.getElementById("latex").value;
+			document.write(email);
+		}
 
 	</script>
 	
