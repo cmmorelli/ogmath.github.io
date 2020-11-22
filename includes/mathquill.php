f982eeafd248a016cfaf88d18c6fa5f2d212e3b5
root <root@ogmath.com>
2020-11-21 23:30:53 -0600

    Update

--- includes/mathquill.php
+++ includes/mathquill.php
@@ -22,23 +22,27 @@
 
     <body>
 
-<p>Type math here: <span id="math-field"></span></p>
-<p>LaTeX of what you typed: <span id="latex"></span></p>
+	<p>Type math here: <span id="math-field"></span></p>
+	<p>LaTeX of what you typed: <span id="latex"></span></p>
 
-<script>
-var mathFieldSpan = document.getElementById('math-field');
-var latexSpan = document.getElementById('latex');
+	<script>
+		var mathFieldSpan = document.getElementById('math-field');
+		var latexSpan = document.getElementById('latex');
 
-var MQ = MathQuill.getInterface(2); // for backcompat
-var mathField = MQ.MathField(mathFieldSpan, {
-  spaceBehavesLikeTab: true, // configurable
-	    handlers: {
-	        edit: function() { // useful event handlers
-			      latexSpan.textContent = mathField.latex(); // simple API
-			          }
-		  }
-});
-</script>
+		var MQ = MathQuill.getInterface(2); // for backcompat
+		var mathField = MQ.MathField(mathFieldSpan, {
+		  spaceBehavesLikeTab: true, // configurable
+			    handlers: {
+				edit: function() { // useful event handlers
+					      latexSpan.textContent = mathField.latex(); // simple API
+						  }
+				  }
+		});
+	</script>
+
+	<script>
+		console.log(latexSpan)
+	</script>
 
     </body>
 </html>
