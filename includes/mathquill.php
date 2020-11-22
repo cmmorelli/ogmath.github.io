6a15f710a26e4c6daff4a08b709835b856888968
root <root@ogmath.com>
2020-11-21 23:23:10 -0600

    Update

--- includes/mathquill.php
+++ includes/mathquill.php
@@ -22,29 +22,23 @@
 
     <body>
 
-          <!--EXAMPLE COMMON TEXT TRANSFORMED WITH MATHQUILL  -->
-         <p>Solve <span id="problem">f(x) = ax^2 + bx + c + = 0</span>.</p>
-            <script>
-                  var problemSpan = document.getElementById('problem');
-                 MQ.StaticMath(problemSpan);
-            </script> 
+<p>Type math here: <span id="math-field"></span></p>
+<p>LaTeX of what you typed: <span id="latex"></span></p>
 
+<script>
+var mathFieldSpan = document.getElementById('math-field');
+var latexSpan = document.getElementById('latex');
 
-
-
-        <!-- EXAMPLE TO  CREATE AN EDITABLE MATH FIELD  -->
-        <p><span id="answer">x=</span></p>
-        <script>
-            var answerSpan = document.getElementById('answer');
-            var answerMathField = MQ.MathField(answerSpan, {
-                handlers: {
-                edit: function() {
-                    var enteredMath = answerMathField.latex(); // Get entered math in LaTeX format
-                    checkAnswer(enteredMath);
-                }
-                }
-            });
-        </script>
+var MQ = MathQuill.getInterface(2); // for backcompat
+var mathField = MQ.MathField(mathFieldSpan, {
+  spaceBehavesLikeTab: true, // configurable
+	    handlers: {
+	        edit: function() { // useful event handlers
+			      latexSpan.textContent = mathField.latex(); // simple API
+			          }
+		  }
+});
+</script>
 
     </body>
 </html>
