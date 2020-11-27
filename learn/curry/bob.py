9ff33ca8660ee98f3df750831075871820d4310c
mike <mike@t420.hitronhub.home>
2020-11-26 19:08:31 -0600

    c and python

---
+++ learn/curry/bob.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+successful = False
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else:
+    print("Attempted 3 times and failed")
--- learn/curry/hello.c
+++ learn/curry/hello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("Hello World\n");
+int main() {
+	int max;
+	scanf("%d", &max);
+	
+	for (int i = 0; i < max; i++) {
+		printf("%d\n", i);
+	}
+
 	return 0;
 }
-
