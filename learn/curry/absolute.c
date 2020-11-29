e8f61b5821d368fdbb71793e10369d7c65714f8c
Michael Morelli <mike@ogmath.com>
2020-11-29 12:59:39 -0600

    update

---
+++ learn/curry/absolute.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hdddello Cjjjj Language");
+    return 0;
+}
---
+++ learn/curry/for.py
@@ -0,0 +1,5 @@
+prime = 0
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+    
