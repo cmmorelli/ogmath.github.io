ef8ccdff087fa8a560d8972887b7ad45df11b6e8
root <root@ogmath.com>
2020-11-30 16:38:30 -0600

    Update

---
+++ learn/mario/mario.c
@@ -0,0 +1,13 @@
+// This is how you type a comment.
+
+/* Here is an alternative way to comment */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    printf("Hello World\n");
+    return 0;
+}
